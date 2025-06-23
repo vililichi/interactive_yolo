c807ac4ac269f32c28cec2fd0a72646623ba322c
vililichi <keven0230@live.ca>
2025-06-23 16:20:10 -0400

    Annotation avec label studio

--- src/model/interactive_yolo_model/tools/evaluate_model/evaluate_model.py
+++ src/model/interactive_yolo_model/tools/evaluate_model/evaluate_model.py
@@ -1,10 +1,11 @@
 import os
 
+import pycocotools.coco
 from torchmetrics.functional.detection.map import mean_average_precision
 
 import rclpy
 from rclpy.node import Node
-from .....common.python.database_service import DatabaseServices
+from database_service import DatabaseServices
 from ...utils.embedding_generator import EmbeddingGenerator
 from ...utils.model_loader.base_model import base_model
 from ...utils.new_object_detection_helper import extract_resized_masks
@@ -13,18 +14,18 @@ from threading import Thread
 
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
 import json
 import time
 
 import cv2
+from matplotlib import pyplot as plt
 
-import base64
-from io import BytesIO
-from PIL import Image
+import pycocotools
 import numpy as np
 
+from typing import Tuple, List
 
+from .rle_decode import rle_to_mask
 
 class TaskNode(Node):
     def __init__(self):
@@ -48,63 +49,64 @@ class TaskNode(Node):
             os.makedirs(self.output_path)
             print(f"Created output directory: {self.output_path}")
 
-    def evaluate_model(self):
-        # Load input
-        input_files = [f for f in os.listdir(self.input_path) if f.endswith('.json')]
-        
+    def get_data_mscoco(self)->Tuple[List[np.ndarray],List[dict],dict]:
+
+        annotation_path = os.path.join(self.input_path, "ann.json")
+        images_dir = os.path.join(self.input_path, "images")
+
         labels_to_id = dict()
-        targets = list()
-        images = list()
+        images = []
+        targets = []
 
-        for input_file in input_files:
-            input_file_path = os.path.join(self.input_path, input_file)
-            print(f"Processing input file: {input_file_path}")
+        dataset = pycocotools.coco.COCO(annotation_path)
 
-            # Load the input data
-            with open(input_file_path, 'r') as f:
-                input_data = json.load(f)
+        img_ids = dataset.getImgIds()
+        for img_id in img_ids:
+            
+            anns_ids = dataset.getAnnIds(imgIds=img_id)
 
-            image_file = input_data['imagePath']
-            image_path = os.path.join(self.input_path, image_file)
-            if not os.path.exists(image_path):
-                print(f"Image file {image_file} does not exist, skipping.")
+            img = dataset.loadImgs(img_id)[0]
+            anns = dataset.loadAnns(anns_ids)
+
+            file_name = img["file_name"]
+            img_path = os.path.join(images_dir, file_name)
+
+            if not os.path.exists(img_path):
+                print(f"Image file {file_name} does not exist, skipping.")
                 continue
-            image = cv2.imread(image_path)
-            w = input_data["imageWidth"]
-            h = input_data["imageHeight"]
+            image = cv2.imread(img_path)
+            h, w, c = image.shape
 
             boxes = list()
             masks = list()
             labels = list()
 
-            for shape in input_data['shapes']:
+            for ann in anns:
 
                 # extract label
-                label = shape['label']
+                cat_id = ann['category_id']
+                cat = dataset.loadCats(cat_id)[0]
+                label = cat['name']
                 if label not in labels_to_id:
                     labels_to_id[label] = len(labels_to_id)
                 label_id = labels_to_id[label]
 
-                # extract mask
-                image_bytes = base64.b64decode(shape['mask'])
-                mask_image = Image.open(BytesIO(image_bytes))
-                mask_located = np.array(mask_image)
-                mask_located = mask_located.astype(bool)
+                # extract bbox
+                x1,y1,xl,yl = ann['bbox']
+                x2 = x1+xl
+                y2 = y1+yl
 
-                x1,y1 = shape['points'][0]
-                x2,y2 = shape['points'][1]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                mask: np.ndarray = np.zeros((h, w), dtype=bool)
-                mask[y1:(y2+1), x1:(x2+1)] = mask_located
+                # extract mask
+                mask = dataset.annToMask(ann)
 
                 # save data
                 boxes.append(torch.FloatTensor([x1, y1, x2, y2]).cpu())
                 masks.append(torch.BoolTensor(mask).cpu())
                 labels.append(label_id)
-    
+
             # add data to targets
             if len(boxes) == 0:
-                print(f"No valid shapes found in {input_file}, skipping.")
+                print(f"No valid shapes found in {file_name}, skipping.")
                 continue
             boxes = torch.stack(boxes, dim=0)
             masks = torch.stack(masks, dim=0)
@@ -115,6 +117,90 @@ class TaskNode(Node):
             })
             images.append(image)
 
+
+
+        return images, targets, labels_to_id
+
+    def get_data_label_studio(self)->Tuple[List[np.ndarray],List[dict],dict]:
+
+        annotation_path = os.path.join(self.input_path, "ann.json")
+        images_dir = os.path.join(self.input_path, "images")
+
+        labels_to_id = dict()
+        images = []
+        targets = []
+
+        # Open and read the JSON file
+        with open(annotation_path, 'r') as file:
+            data: dict = json.load(file)
+
+        for img_data in data:
+
+            #open img
+            file_name = img_data["file_upload"]
+            img_path = os.path.join(images_dir, file_name)
+
+            if not os.path.exists(img_path):
+                print(f"Image file {file_name} does not exist, skipping.")
+                continue
+            image = cv2.imread(img_path)
+            h, w, c = image.shape
+
+            #open annotations
+            anns_data:dict = img_data["annotations"][0]["result"]
+
+            #parse annotations
+            boxes = list()
+            masks = list()
+            labels = list()
+            for ann_data in anns_data:
+
+                if(ann_data["type"] != "brushlabels"):
+                    continue
+
+                # extract label
+                label = ann_data["value"]["brushlabels"][0]
+                if label not in labels_to_id:
+                    labels_to_id[label] = len(labels_to_id)
+                label_id = labels_to_id[label]
+
+                
+
+                # extract mask
+                rle_mask = ann_data["value"]["rle"]
+                mask = rle_to_mask(rle_mask, h, w)
+
+                # extract bbox
+                xs = np.any(mask, axis=1)
+                ys = np.any(mask, axis=0)
+                y1, y2 = np.where(ys)[0][[0, -1]]
+                x1, x2 = np.where(xs)[0][[0, -1]]
+                print(x1,y1,x2,y2)
+
+                # save data
+                boxes.append(torch.FloatTensor([x1, y1, x2, y2]).cpu())
+                masks.append(torch.BoolTensor(mask).cpu())
+                labels.append(label_id)
+
+            # add data to targets
+            if len(boxes) == 0:
+                print(f"No valid shapes found in {file_name}, skipping.")
+                continue
+            boxes = torch.stack(boxes, dim=0)
+            masks = torch.stack(masks, dim=0)
+            targets.append({
+                'boxes': boxes,
+                'masks': masks,
+                'labels': torch.IntTensor(labels).cpu()
+            })
+            images.append(image)
+
+        return images, targets, labels_to_id
+
+    def evaluate_model(self):
+        # Load input
+        images, targets, labels_to_id = self.get_data_label_studio()
+
         if len(targets) == 0:
             print("No valid targets found, exiting.")
             return
@@ -123,74 +209,56 @@ class TaskNode(Node):
         label_str_list = list(labels_to_id.keys())
         self.ref_model.set_classes(label_str_list, self.ref_model.get_text_pe(label_str_list))
 
-        # Load the model learning
-        self.embeddings_generator.update()
-        embeddings, alias_name_list, category_alias_to_name = self.embeddings_generator.get_embedding(categories_name=label_str_list, fallback_model=self.trained_model)
-
-        if len(embeddings) > 0:
-            self.trained_model.set_classes(alias_name_list, embeddings)
-        
         # Load the model learning with all classes
-        ncs_embeddings, ncs_alias_name_list, ncs_category_alias_to_name = self.embeddings_generator.get_embedding(categories_name=[], fallback_model=self.trained_model_with_no_classes_specification)
-        if len(ncs_embeddings) > 0:
-            self.trained_model_with_no_classes_specification.set_classes(ncs_alias_name_list, ncs_embeddings)
+        self.embeddings_generator.update()
+        embeddings, alias_name_list, category_alias_to_name, alias_score_exponent_list = self.embeddings_generator.get_embedding(categories_name=None, fallback_model=self.trained_model_with_no_classes_specification)
+        if len(embeddings) > 0:
+            self.trained_model_with_no_classes_specification.set_classes(alias_name_list, embeddings)
 
         # Evaluate the model
         print("Evaluating model...")
 
-        trained_results = self.trained_model.predict(images, conf=0.05, verbose=False)
-        trained_ncs_results = self.trained_model_with_no_classes_specification.predict(images, conf=0.05, verbose=False)
+        trained_results = self.trained_model_with_no_classes_specification.predict(images, conf=0.05, verbose=False)
         ref_results = self.ref_model.predict(images, conf=0.05, verbose=False)
 
         trained_preds = []
-        trained_ncs_preds = []
+        trained_with_conf_learning_preds = []
         ref_preds = []
 
         for i in range(len(images)):
-            trained_result = trained_results[i]
             ref_result = ref_results[i]
+            trained_result = trained_results[i]
 
-            # Convert results to the format expected by MeanAveragePrecision
+            # trained result extraction
             trained_boxes = trained_result.boxes.xyxy.cpu()
             trained_conf = trained_result.boxes.conf.cpu()
+            
             trained_masks = extract_resized_masks(trained_result).cpu()
             trained_labels_nbr = trained_result.boxes.cls.size()[0]
-            trained_labels_alias = [trained_result.names[trained_result.boxes.cls[i].item()] for i in range(trained_labels_nbr) ]
-            trained_labels_names = [category_alias_to_name[trained_labels_alias[i]] for i in range(trained_labels_nbr)]
+            trained_labels_alias = [trained_result.names[trained_result.boxes.cls[j].item()] for j in range(trained_labels_nbr) ]
+            trained_labels_names = [category_alias_to_name[trained_labels_alias[j]] for j in range(trained_labels_nbr)]
+
+            trained_conf_corrected = torch.tensor([ x.item() ** alias_score_exponent_list[int(trained_result.boxes.cls[j].item())] for j, x in enumerate(torch.unbind(trained_conf))])
+
+            valid_ids = [j for j in range(trained_labels_nbr) if trained_labels_names[j] in labels_to_id]
+            trained_labels_names = [trained_labels_names[j] for j in valid_ids]
+            trained_boxes = trained_boxes[valid_ids]
+            trained_masks = trained_masks[valid_ids]
+            trained_conf = trained_conf[valid_ids]
+            trained_conf_corrected = trained_conf_corrected[valid_ids]
             trained_labels_ids = [labels_to_id[name] for name in trained_labels_names]
             trained_labels_ids_tensor = torch.IntTensor(trained_labels_ids).cpu()
 
-            trained_ncs_boxes = trained_ncs_results[i].boxes.xyxy.cpu()
-            trained_ncs_conf = trained_ncs_results[i].boxes.conf.cpu()
-            trained_ncs_masks = extract_resized_masks(trained_ncs_results[i]).cpu()
-            trained_ncs_labels_nbr = trained_ncs_results[i].boxes.cls.size()[0]
-            trained_ncs_labels_alias = [trained_ncs_results[i].names[trained_ncs_results[i].boxes.cls[j].item()] for j in range(trained_ncs_labels_nbr) ]
-            trained_ncs_labels_names = [ncs_category_alias_to_name[trained_ncs_labels_alias[j]] for j in range(trained_ncs_labels_nbr)]
-
-            valid_ids = [i for i in range(trained_ncs_labels_nbr) if trained_ncs_labels_names[i] in labels_to_id]
-            trained_ncs_labels_names = [trained_ncs_labels_names[i] for i in valid_ids]
-            trained_ncs_boxes = trained_ncs_boxes[valid_ids]
-            trained_ncs_masks = trained_ncs_masks[valid_ids]
-            trained_ncs_conf = trained_ncs_conf[valid_ids]
-
-            trained_ncs_labels_ids = [labels_to_id[name] for name in trained_ncs_labels_names]
-            trained_ncs_labels_ids_tensor = torch.IntTensor(trained_ncs_labels_ids).cpu()
-
+            # Ref result extraction
             ref_boxes = ref_result.boxes.xyxy.cpu()
             ref_conf = ref_result.boxes.conf.cpu()
             ref_masks = extract_resized_masks(ref_result).cpu()
             ref_labels_nbr = ref_result.boxes.cls.size()[0]
-            ref_labels_names = [ref_result.names[ref_result.boxes.cls[i].item()] for i in range(ref_labels_nbr) ]
-            ref_labels_ids = [labels_to_id[ref_labels_names[i]] for i in range(ref_labels_nbr) ]
+            ref_labels_names = [ref_result.names[ref_result.boxes.cls[j].item()] for j in range(ref_labels_nbr) ]
+            ref_labels_ids = [labels_to_id[name] for name in ref_labels_names ]
             ref_labels_ids_tensor = torch.IntTensor(ref_labels_ids).cpu()
 
             # Append predictions
-            trained_preds.append({
-                'boxes': trained_boxes,
-                'masks': trained_masks,
-                'labels': trained_labels_ids_tensor,
-                'scores': trained_conf
-            })
 
             ref_preds.append({
                 'boxes': ref_boxes,
@@ -199,11 +267,18 @@ class TaskNode(Node):
                 'scores': ref_conf
             })
 
-            trained_ncs_preds.append({
-                'boxes': trained_ncs_boxes,
-                'masks': trained_ncs_masks,
-                'labels': trained_ncs_labels_ids_tensor,
-                'scores': trained_ncs_conf
+            trained_preds.append({
+                'boxes': trained_boxes,
+                'masks': trained_masks,
+                'labels': trained_labels_ids_tensor,
+                'scores': trained_conf
+            })
+
+            trained_with_conf_learning_preds.append({
+                'boxes': trained_boxes,
+                'masks': trained_masks,
+                'labels': trained_labels_ids_tensor,
+                'scores': trained_conf_corrected
             })
 
         map_ref_seg = mean_average_precision(
@@ -218,8 +293,8 @@ class TaskNode(Node):
             iou_type="segm",
             class_metrics=True
         )
-        map_trained_ncs_seg = mean_average_precision(
-            preds=trained_ncs_preds,
+        map_trained_with_conf_learning_seg = mean_average_precision(
+            preds=trained_with_conf_learning_preds,
             target=targets,
             iou_type="segm",
             class_metrics=True
@@ -237,8 +312,8 @@ class TaskNode(Node):
             iou_type="bbox",
             class_metrics=True
         )
-        map_trained_ncs_bbox = mean_average_precision(
-            preds=trained_ncs_preds,
+        map_trained_with_conf_learning_bbox = mean_average_precision(
+            preds=trained_with_conf_learning_preds,
             target=targets,
             iou_type="bbox",
             class_metrics=True
@@ -295,26 +370,26 @@ class TaskNode(Node):
                         'map_per_class': map_trained_bbox['map_per_class'].cpu().tolist()
                     }
                 },
-                'database helped model with no classes specification performances': {
+                'database helped model with confidence learning performances': {
                     "segmentation":
                     {
-                        'map': map_trained_ncs_seg['map'].item(),
-                        'map_50': map_trained_ncs_seg['map_50'].item(),
-                        'map_75': map_trained_ncs_seg['map_75'].item(),
-                        'map_small': map_trained_ncs_seg['map_small'].item(),
-                        'map_medium': map_trained_ncs_seg['map_medium'].item(),
-                        'map_large': map_trained_ncs_seg['map_large'].item(),
-                        'map_per_class': map_trained_ncs_seg['map_per_class'].cpu().tolist()
+                        'map': map_trained_with_conf_learning_seg['map'].item(),
+                        'map_50': map_trained_with_conf_learning_seg['map_50'].item(),
+                        'map_75': map_trained_with_conf_learning_seg['map_75'].item(),
+                        'map_small': map_trained_with_conf_learning_seg['map_small'].item(),
+                        'map_medium': map_trained_with_conf_learning_seg['map_medium'].item(),
+                        'map_large': map_trained_with_conf_learning_seg['map_large'].item(),
+                        'map_per_class': map_trained_with_conf_learning_seg['map_per_class'].cpu().tolist()
                     },
                     "bounding boxes":
                     {
-                        'map': map_trained_ncs_bbox['map'].item(),
-                        'map_50': map_trained_ncs_bbox['map_50'].item(),
-                        'map_75': map_trained_ncs_bbox['map_75'].item(),
-                        'map_small': map_trained_ncs_bbox['map_small'].item(),
-                        'map_medium': map_trained_ncs_bbox['map_medium'].item(),
-                        'map_large': map_trained_ncs_bbox['map_large'].item(),
-                        'map_per_class': map_trained_ncs_bbox['map_per_class'].cpu().tolist()
+                        'map': map_trained_with_conf_learning_bbox['map'].item(),
+                        'map_50': map_trained_with_conf_learning_bbox['map_50'].item(),
+                        'map_75': map_trained_with_conf_learning_bbox['map_75'].item(),
+                        'map_small': map_trained_with_conf_learning_bbox['map_small'].item(),
+                        'map_medium': map_trained_with_conf_learning_bbox['map_medium'].item(),
+                        'map_large': map_trained_with_conf_learning_bbox['map_large'].item(),
+                        'map_per_class': map_trained_with_conf_learning_bbox['map_per_class'].cpu().tolist()
                     }
                 }
             }, f, indent=4)
